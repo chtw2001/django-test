1c6781b12f0a8231863ef71f6dd4ceca3946b32c
chtw2001 <jungtaekwon1019@gmail.com>
2023-04-30 23:28:41 +0900

    add foreignkey to user

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 # users 폴더 생성후 INSTALLED_APPS에 추가하고 models.py에 모델을 정의하고 AUTH_USER_MODEL을 설정후
 # makeigrations, migrate 하면 오류 발생 => 이미 모델이 만들어져있기 때문임 => sqlite3 삭제후 migrate
+# sqlite3 extention 설치 후 createsuperuser => users_user에 만들어졌는지 확인
--- users/models.py
+++ users/models.py
@@ -3,3 +3,8 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     name = models.CharField(verbose_name = '이름', max_length=10, unique=True)
+    
+    
+class UserInfo(models.Model):
+    age = models.IntegerField(verbose_name = '나이')
+    user = models.ForeignKey(to = 'User', on_delete=models.CASCADE)
